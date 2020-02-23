5c8ba0932034a07224510ed6e5487475b9873d67
raphkm <rkariuki415@gmail.com>
2020-02-23 23:15:49 +0300

    added new changes to Readme file, Html & JavaScript

---
+++ README.md
@@ -0,0 +1,38 @@
+
+
+# Project Name:
+>## Delani studio.
+
+# Project Description:
+>## This is a  recreation of a website for an organisation known as Delani Studios whose goal is to get customers who are interested in making their own websites at a cost.
+
+> <img src="https://delaniimaya.netlify.com/">
+
+# Author(s) information:
+># Raphael Kariuki
+
+# Installation instructions:
+*Open the live link to view the project, Here is the live link :"
+
+# Technologies used:
+* HTML,
+* CSS,
+* Bootstrap,
+* JavaScript,
+* Jquery.
+
+# Contact information.
+> For more information contact my email- rkariuki415@gmail.com
+
+# License and Copyright information.
+> 
+License
+MIT License
+
+Copyright (c) 2020 Raphael Kariuki.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ script.js
@@ -0,0 +1,112 @@
+// submit button function
+function validateTheForm() {
+    var Name = document.getElementById("Name").value;
+    var Email = document.getElementById("Email").value;
+    if (Name == "") {
+      alert("please fill in your Name");
+      return false;
+    } 
+    else if (Email == "") {
+      alert("Please fill in your name and Email");
+      return false;
+    }
+     else {
+      alert(
+        Name +
+          ", we have received your message. Thank you for reaching out to us."
+      );
+    }
+    event.preventDefault();
+  }
+//   function for what we do-responsiveness
+  $(document).ready(function() {
+    $("#design-Img").click(function() {
+      $("#dsgn").show("2000");
+      $("#design-Img").hide("slow")
+    });
+    $("#dsgn").click(function() {
+      $("#design-Img").show("2000");
+      $("#dsgn").hide("slow")
+    });
+    $("#develop-Img").click(function() {
+      $("#dvlpmnt").show("2000");
+      $("#develop-Img").hide("slow")
+    });
+    $("#dvlpmnt").click(function() {
+      $("#develop-Img").show("2000");
+      $("#dvlpmnt").hide("slow")
+    });
+    $("#product-Img").click(function() {
+      $("#prdct").show("2000");
+      $("#product-Img").hide("slow")
+    }); 
+    $("#prdct").click(function() {
+      $("#product-Img").show("2000");
+      $("#prdct").hide("slow")
+    });
+//    function for portfolio-responsivesness  
+    $("#work1").hover(
+      function() {
+        $("#txt1").show("2000");
+      },
+      function() {
+        $("#txt1").hide("2000");
+      }
+    );
+    $("#work2").hover(
+      function() {
+        $("#txt2").show("2000");
+      },
+      function() {
+        $("#txt2").hide("2000");
+      }
+    );
+    $("#work3").hover(
+      function() {
+        $("#txt3").show("2000");
+      },
+      function() {
+        $("#txt3").hide("2000");
+      }
+    );
+    $("#work4").hover(
+      function() {
+        $("#txt4").show("2000");
+      },
+      function() {
+        $("#txt4").hide("2000");
+      }
+    );
+    $("#work5").hover(
+      function() {
+        $("#txt5").show("2000");
+      },
+      function() {
+        $("#txt5").hide("2000");
+      }
+    );
+    $("#work6").hover(
+      function() {
+        $("#txt6").show("2000");
+      },
+      function() {
+        $("#txt6").hide("2000");
+      }
+    );
+    $("#work7").hover(
+      function() {
+        $("#txt7").show("2000");
+      },
+      function() {
+        $("#txt7").hide("2000");
+      }
+    );
+    $("#work8").hover(
+      function() {
+        $("#txt8").show("2000");
+      },
+      function() {
+        $("#txt8").hide("2000");
+      }
+    );
+  });
